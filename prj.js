9c380f24451a5859d8f2c9373df9320b8ade504e
Humoyun Abdullazoda <you@example.com>
2022-12-25 17:59:16 +0500

    GM mashinalari obzori

--- prj.js
+++ prj.js
@@ -3,3 +3,8 @@ function fon() {
 	if (main1) main1.className = 'main2';
 	else document.querySelector('div.main2').className = 'main';
 }
+
+function rangBer(rang) {
+	let form = document.querySelector('div.main');
+	form.style.backgroundColor = rang;
+}
